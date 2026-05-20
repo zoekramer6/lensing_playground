03ee417ad24c2ee1c8e4f14f1b3c3cc2216a77e9
Zoe Kramer <zoekramer6@gmail.com>
2026-05-20 16:48:46 -0700

    Update physics.js

--- physics.js
+++ physics.js
@@ -1,11 +1,6 @@
-// physics.js
-
-// This is where all gravitational lensing math will live later
-
 function lensDeflection(dx, dy, strength) {
   let r2 = dx * dx + dy * dy + 0.0001;
 
-  // simple toy model: deflection ~ 1/r^2
   let factor = strength / r2;
 
   return {
