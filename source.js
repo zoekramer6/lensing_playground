4362e1ed1a02a7a010ea5d55e8f9d6680a5ca308
Zoe Kramer <zoekramer6@gmail.com>
2026-05-20 17:41:11 -0700

    Update source.js

--- source.js
+++ source.js
@@ -1,1 +1,13 @@
+let source = [];
 
+function initSource() {
+  for (let i = 0; i < 3000; i++) {
+    let x = (Math.random()-0.5)*300;
+    let y = (Math.random()-0.5)*300;
+
+    let r2 = x*x + y*y;
+    let flux = Math.exp(-r2 / 8000);
+
+    source.push({x,y,flux});
+  }
+}
