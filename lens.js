5a50d8cc8971cd0685ce7999086d928b399af817
Zoe Kramer <zoekramer6@gmail.com>
2026-05-20 17:41:23 -0700

    Update lens.js

--- lens.js
+++ lens.js
@@ -1,1 +1,13 @@
+let lensX, lensY;
 
+function initLens() {
+  lensX = window.innerWidth/2;
+  lensY = window.innerHeight/2;
+
+  window.addEventListener("mousemove", (e) => {
+    if (e.buttons) {
+      lensX = e.clientX;
+      lensY = e.clientY;
+    }
+  });
+}
